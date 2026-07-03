9412ea0b33aea5f931f8e4018c1af1a489d16ad4
ToskaK6 <185360252+ToskaK6@users.noreply.github.com>
2026-07-03 19:35:52 +0200

    Update applicationApi.js -R

--- frontend/src/api/applicationApi.js
+++ frontend/src/api/applicationApi.js
@@ -3,6 +3,7 @@ import api from "./axios";
 export const applicationApi = {
   create: (data) => api.post("/applications", data),
   listMine: (params) => api.get("/applications", { params }),
+  listLandlord: (params) => api.get("/applications/landlord", { params }),
   listForProperty: (propertyId, params) => api.get(`/applications/property/${propertyId}`, { params }),
   updateStatus: (id, status) => api.put(`/applications/${id}/status`, { status }),
 };
