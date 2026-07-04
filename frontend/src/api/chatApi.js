5fd78d6441cc760805d04a6d6c66c8474aacbce0
ToskaK6 <185360252+ToskaK6@users.noreply.github.com>
2026-07-04 19:06:37 +0200

    fixed chat bug in chat api -R

--- frontend/src/api/chatApi.js
+++ frontend/src/api/chatApi.js
@@ -2,4 +2,6 @@ import api from "./axios";
 
 export const chatApi = {
   getPartners: (propertyId) => api.get(`/chat/property/${propertyId}/partners`),
+  getMessages: (propertyId, otherUserId) =>
+    api.get(`/chat/property/${propertyId}/messages`, { params: { other_user_id: otherUserId } }),
 };
